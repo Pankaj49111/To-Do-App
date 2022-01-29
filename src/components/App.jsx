29f84e66e673fa78a9e1967c3029e1c8498b9b25
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2022-01-29 15:48:32 +0530

    can remove entries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,55 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import TodoItem from './ToDoItems'
 
 function App() {
-  const [inputText, setInputText] = useState("");
-  const [items, setItems] = useState([]);
+  const [inputText, setInputText] = useState('')
+  const [items, setItems] = useState([])
 
   function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
+    const newValue = event.target.value
+    setInputText(newValue)
   }
 
-  function handleClick() {
+  function addItem() {
     setItems((prevItems) => {
-      return [...prevItems, inputText];
-    });
-    setInputText("");
+      return [...prevItems, inputText]
+    })
+    setInputText('')
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id
+      })
+    })
   }
 
   return (
-    <div className="container">
-      <div className="heading">
+    <div className='container'>
+      <div className='heading'>
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input type="text" onChange={handleChange} value={inputText} />
-        <button onClick={handleClick}>
+      <div className='form'>
+        <input type='text' onChange={handleChange} value={inputText} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {items.map((todoItem) => (
-            <li>{todoItem}</li>
+          {items.map((todoItem, index) => (
+            <TodoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
           ))}
         </ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
